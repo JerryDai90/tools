2cf9b38e95d76376dda5684bc24059fdce4314c7
JerryDai90 <JerryDai90@gmail.com>
2020-02-27 21:43:17 +0800

    增加注释

--- shell/src/test/java/fun/lsof/tools/shell/ShellUtilTest.java
+++ shell/src/test/java/fun/lsof/tools/shell/ShellUtilTest.java
@@ -9,15 +9,7 @@ public class ShellUtilTest {
     @Test
     public void testSSH() {
 
-//        if (null != args && args.length >= 1) {
-//            cmd = "conf###rule id 103###enable###exit".split("###");
-//        } else {
-//            cmd = "conf###rule id 103###disable###exit".split("###");
-//        }
-
-//        t.sshKeyblock("hillstone", "192.168.254.5", "gdgm*&&)6270", cmd);
-
-        ShellResult root = ShellUtil.run("root", "192.168.29.194", "1qaz@WSX@2019", 22, new String[]{"ll", "ls", "pwd"});
+        ShellResult root = ShellUtil.run("root", "192.168.29.194", "1qaz", 22, new String[]{"ll", "ls", "pwd"});
 
         System.out.println(root);
 
